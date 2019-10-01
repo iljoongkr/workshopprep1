a681d3f1ba578c46050db3ebb8bef9d6a7293e16
Phillip LeBlanc <phillip.leblanc@microsoft.com>
2019-10-01 16:23:41 +0900

    Adding App Center SDKs

--- WorkshopPrepApp/App.xaml.cs
+++ WorkshopPrepApp/App.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace WorkshopPrepApp
 {
@@ -15,7 +21,9 @@ namespace WorkshopPrepApp
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            AppCenter.Start("ios=555570a9-313c-4167-bbcb-498a41efab6d;" +
+                  "android=30c218f5-e91b-4da9-ae8f-bef98a51fd87",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
